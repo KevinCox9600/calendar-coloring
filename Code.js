5ff1d790ec39f1985896715436c95e77c17e3bdf
Kevin Cox <kevindanielcox@gmail.com>
2022-02-11 17:59:47 -0500

    Final change attempts

--- Code.js
+++ Code.js
@@ -5,23 +5,31 @@
 function init() {
     var syncToken;
     var page = Calendar.Events.list('primary', {});
+    console.log(page);
     if (page.items && page.items.length > 0) {
         syncToken = page.nextSyncToken;
+        // console.log(page.nextSyncToken);
     }
 
-    PropertiesService.getUserProperties().setProperty('SYNC_TOKEN', syncToken);
+    console.log(syncToken);
+    console.log(PropertiesService.getUserProperties().getProperties());
+    if (syncToken) {
+        PropertiesService.getUserProperties().setProperty('SYNC_TOKEN', syncToken);
+    }
 
     ScriptApp.newTrigger('calendarSync')
         .forUserCalendar(Session.getEffectiveUser().getEmail())
         .onEventUpdated()
         .create();
 }
+
 function calendarSync(e) {
     var syncToken = PropertiesService.getUserProperties().getProperty('SYNC_TOKEN');
     var page = Calendar.Events.list('primary', { syncToken: syncToken });
     if (page.items && page.items.length > 0) {
         for (var i = 0; i < page.items.length; i++) {
             var item = page.items[i];
+            console.log("hello");
             console.log(JSON.stringify(item));
             // Do what you want
         }
