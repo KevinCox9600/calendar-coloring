8183c5758a307aec4679dc45d4a648dc44c5a03b
Kevin Cox <kevindanielcox@gmail.com>
2022-02-11 18:52:40 -0500

    Move code into function

--- Code.js
+++ Code.js
@@ -1,9 +1,8 @@
-var calendar = CalendarApp.getCalendarById('putYourCalendarIdHere:String');
-// begin one week before, end one year after
-var beginDate = new Date(new Date().getTime() - (7 * 86400000));
-var endDate = new Date(new Date().getTime() + (52 * 7 * 86400000));
-
 function assign_color() {
+  var calendar = CalendarApp.getDefaultCalendar();
+  // begin one week before, end one year after
+  var beginDate = new Date(new Date().getTime() - (7 * 86400000));
+  var endDate = new Date(new Date().getTime() + (52 * 7 * 86400000));
   var events = calendar.getEvents(beginDate, endDate);
   for (var i = 0; i < events.length; i++) {
     var event = events[i];
@@ -16,8 +15,4 @@ function assign_color() {
       event.setColor('9');
     } else { }
   }
-}
-
-function doPost() {
-  assign_color();
 }
