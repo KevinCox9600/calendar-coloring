aecfe1bd97025a3b20be1e547793c1d58147a50e
Kevin Cox <kevindanielcox@gmail.com>
2022-02-11 18:01:15 -0500

    Add code to assign colors

--- Code.js
+++ Code.js
@@ -1,39 +1,14 @@
-// function main() {
-//     const calendar = CalendarApp.getDefaultCalendar();
-// }
-
-function init() {
-    var syncToken;
-    var page = Calendar.Events.list('primary', {});
-    console.log(page);
-    if (page.items && page.items.length > 0) {
-        syncToken = page.nextSyncToken;
-        // console.log(page.nextSyncToken);
-    }
-
-    console.log(syncToken);
-    console.log(PropertiesService.getUserProperties().getProperties());
-    if (syncToken) {
-        PropertiesService.getUserProperties().setProperty('SYNC_TOKEN', syncToken);
-    }
-
-    ScriptApp.newTrigger('calendarSync')
-        .forUserCalendar(Session.getEffectiveUser().getEmail())
-        .onEventUpdated()
-        .create();
-}
-
-function calendarSync(e) {
-    var syncToken = PropertiesService.getUserProperties().getProperty('SYNC_TOKEN');
-    var page = Calendar.Events.list('primary', { syncToken: syncToken });
-    if (page.items && page.items.length > 0) {
-        for (var i = 0; i < page.items.length; i++) {
-            var item = page.items[i];
-            console.log("hello");
-            console.log(JSON.stringify(item));
-            // Do what you want
-        }
-        syncToken = page.nextSyncToken;
-    }
-    PropertiesService.getUserProperties().setProperty('SYNC_TOKEN', syncToken);
+function assign_color() {
+  var events = calendar.getEvents(beginDate, endDate);
+  for (var i = 0; i < events.length; i++) {
+    var event = events[i];
+    var title = event.getTitle();
+    if (/Submission:/.test(title)) {
+      event.setColor('3');
+    } else if (/Test:/.test(title)) {
+      event.setColor('5');
+    } else if (/Subscription:/.test(title)) {
+      event.setColor('9');
+    } else { }
+  }
 }
