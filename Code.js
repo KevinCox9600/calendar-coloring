be1d633ff88b74f82343a31a53c63d82b41ff690
Kevin Cox <kevindanielcox@gmail.com>
2022-02-12 01:46:50 -0500

    Characters must start line for regex

--- Code.js
+++ Code.js
@@ -2,42 +2,42 @@ const MS_IN_DAY = 24 * 60 * 60 * 1000;
 const COLORS = CalendarApp.EventColor;
 const textColorPairs = [
   {
-    regex: /[Ww] /, // Work
+    regex: /^[Ww] /, // Work
     remove: true,
     color: COLORS.BLUE,
   },
   {
-    regex: /[Ss] /, // Social
+    regex: /^[Ss] /, // Social
     remove: true,
     color: COLORS.CYAN,
   },
   {
-    regex: /[Pp] /, // Productive
+    regex: /^[Pp] /, // Productive
     remove: true,
     color: COLORS.GREEN,
   },
   {
-    regex: /[Ee] /, // Exercise
+    regex: /^[Ee] /, // Exercise
     remove: true,
     color: COLORS.PALE_GREEN,
   },
   {
-    regex: /[Cc] /, // Class
+    regex: /^[Cc] /, // Class
     remove: true,
     color: COLORS.PALE_RED,
   },
   {
-    regex: /[Ff] /, // Fudge I just wasted time
+    regex: /^[Ff] /, // Fudge I just wasted time
     remove: true,
     color: COLORS.ORANGE,
   },
   {
-    regex: /[Gg] /, // Gray
+    regex: /^[Gg] /, // Gray
     remove: true,
     color: COLORS.GRAY,
   },
   {
-    regex: /[Aa] /, // Alert
+    regex: /^[Aa] /, // Alert
     remove: true,
     color: COLORS.RED,
   },
