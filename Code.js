df0211b5f9524a192aeb4438aaf25acaabf486d0
Kevin Cox <kevindanielcox@gmail.com>
2022-09-15 13:58:46 -0400

    Add colors

--- Code.js
+++ Code.js
@@ -1,53 +1,97 @@
 const MS_IN_DAY = 24 * 60 * 60 * 1000;
 const COLORS = CalendarApp.EventColor;
+const DAYS_BEFORE = 7 * MS_IN_DAY;
+const DAYS_AFTER = 3 * 30 * MS_IN_DAY; // 3 months
 const textColorPairs = [
+  // Default color changes
   {
-    regex: /^[Ww] /, // Work
+    regex: /^w /i, // Work
     remove: true,
     color: COLORS.BLUE,
   },
   {
-    regex: /^[Ss] /, // Social
+    regex: /^s /i, // Social
     remove: true,
     color: COLORS.CYAN,
   },
   {
-    regex: /^[Pp] /, // Productive
+    regex: /^p /i, // Productive
     remove: true,
     color: COLORS.GREEN,
   },
   {
-    regex: /^[Ee] /, // Exercise
+    regex: /^e /i, // Exercise
     remove: true,
     color: COLORS.PALE_GREEN,
   },
   {
-    regex: /^[Cc] /, // Class
+    regex: /^c /i, // Class
     remove: true,
     color: COLORS.PALE_RED,
   },
   {
-    regex: /^[Ff] /, // Fudge I just wasted time
+    regex: /^f /i, // Fudge I just wasted time
     remove: true,
     color: COLORS.ORANGE,
   },
   {
-    regex: /^[Gg] /, // Gray
+    regex: /^g /i, // Gray
     remove: true,
     color: COLORS.GRAY,
   },
   {
-    regex: /^[Aa] /, // Alert
+    regex: /^a /i, // Alert
     remove: true,
     color: COLORS.RED,
   },
+  // Keyword color changes
+  {
+    regex: /^sec\b/i, // Security (sec)
+    remove: false,
+    color: COLORS.BLUE
+  },
+  {
+    regex: /^22\b/i, // CS22
+    remove: false,
+    color: COLORS.BLUE
+  },
+  {
+    regex: /^((germ)|(grmn))\b/i, // german (germ/grmn)
+    remove: false,
+    color: COLORS.BLUE
+  },
+  {
+    regex: /^(musc)\b/i, // music theory (musc)
+    remove: false,
+    color: COLORS.BLUE
+  },
+  {
+    regex: /^-/i, // assignment
+    remove: false,
+    color: COLORS.RED
+  }
 ];
+addColorRule('emb', 'blue', true, false);
+addColorRule('ds', 'blue', true, false);
+addColorRule('eng', 'blue', true, false);
+addColorRule('sign', 'blue', true, false);
+addColorRule('cl', 'blue', true, false);
+
+/** Generate the object to represent coloring texts. */
+function addColorRule(text, color = 'red', useBoundary = false, remove = false) {
+  const rule = {
+    regex: new RegExp(`/^${text + useBoundary ? '\\b' : ''}`),
+    remove,
+    color: COLORS[color.toUpperCase()]
+  };
+  textColorPairs.push(rule);
+}
 
 function assign_color() {
   var calendar = CalendarApp.getDefaultCalendar();
   // begin one week before, end one year after
-  var beginDate = new Date(new Date().getTime() - (7 * MS_IN_DAY));
-  var endDate = new Date(new Date().getTime() + (52 * 7 * MS_IN_DAY));
+  var beginDate = new Date(new Date().getTime() - (DAYS_BEFORE));
+  var endDate = new Date(new Date().getTime() + (DAYS_AFTER));
   var events = calendar.getEvents(beginDate, endDate);
 
   for (let i = 0; i < events.length; i++) {
